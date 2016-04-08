dde890fdaf749e1f3d0293ead6cbdf25a80fd165
Mezoka <c823@mail.com>
2016-04-09 01:17:29 +0800

    Update board.h

--- board.h
+++ board.h
@@ -15,44 +15,44 @@ class Board
 {
 public:
 
-	enum
-	{
+    enum
+    {
         TOKEN_NONE, TOKEN_BLACK, TOKEN_WHITE, TOKEN_ADD, TOKEN_ADD_BLACK, TOKEN_ADD_WHITE, TOKEN_PLAY,
         TOKEN_TAKE, TOKEN_PASS, TOKEN_LABEL, TOKEN_NUMBER, TOKEN_MARK, TOKEN_TRIANGLE, TOKEN_CIRCLE,
         TOKEN_SQUARE, TOKEN_PLAY_BLACK, TOKEN_PLAY_WHITE, TOKEN_COMMENT, TOKEN_TIME, TOKEN_EVENT,
         TOKEN_PLACE, TOKEN_DATE, TOKEN_RESULT, TOKEN_SIZE, TOKEN_KOMI, TOKEN_HANDICAP
-	};
+    };
 
-	struct Property
-	{
+    struct Property
+    {
         int Label, Value;
         int Row, Col;
-	};
+    };
 
-	struct Node
-	{
-		std::vector<Property> Prop;
+    struct Node
+    {
+        std::vector<Property> Prop;
         int Turn, Pass, Ko;
         QString Text;
-	};
+    };
 
-	void Clear();
-	void Reset(int x = 19, int y = 19);
-	void Print();
+    void Clear();
+    void Reset(int x = 19, int y = 19);
+    void Print();
 
-	int OtherColor(int color);
-	int Play(int x, int y, int color);
+    int OtherColor(int color);
+    int Play(int x, int y, int color);
     int Add(int x, int y, int color);
     int Append(int x, int y, int color);
     int Read(QString Path, int k = 0);
     int Write(QString Path);
-	int CheckBound(int x, int y);
-	int GetColor(int x, int y);
+    int CheckBound(int x, int y);
+    int GetColor(int x, int y);
 
     void Capture(int x, int y, int color, Node *node);
     void Check(int x, int y, int color, Node *node);
     void CheckAll(int x, int y, int color, Node *node);
-	int CheckLiberty(int x, int y, int color);
+    int CheckLiberty(int x, int y, int color);
     void TreeCut();
 
     int GetAlpha(int x, int y);
@@ -67,11 +67,11 @@ public:
     int PrevNode(int k = 1);
     int NextNode(int k = 1);
 
-	int Point[MAX_BOARD][MAX_BOARD];
+    int Point[MAX_BOARD][MAX_BOARD];
     int Mark[MAX_BOARD][MAX_BOARD];
     int Area[MAX_BOARD][MAX_BOARD];
 
-	int Width, Height, Size;
+    int Width, Height, Size;
     int Turn, Pass, Mode, Path;
     int Index, Index2;
     int Star[18];
